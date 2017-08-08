1abd3fc56024e31d6058b311b08f8d834b9038f2
AbhishekSondkar <30792208+AbhishekSondkar@users.noreply.github.com>
2017-08-08 19:47:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName' ,function(req,res){
-   res.send(createTemplate(articleName));
+   res.send(createTemplate(articles(articleName)));
 });
 
 
