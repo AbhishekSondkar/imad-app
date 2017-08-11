4f38938700746e3b381816b49df6a37a1fe1b273
AbhishekSondkar <30792208+AbhishekSondkar@users.noreply.github.com>
2017-08-11 12:28:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,8 +87,14 @@ app.get('/:articleName' ,function(req,res){
 
 
 
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
