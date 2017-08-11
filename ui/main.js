46cbef2a801b00566db6bb667c5b3fd43e236de2
AbhishekSondkar <30792208+AbhishekSondkar@users.noreply.github.com>
2017-08-11 13:00:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,7 @@ var marginLeft = 0;
 function moveLeft()
 {
     marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginleft + 'px';
 }
 img.onclick = function (){
     var interval = setInterval(moveLeft , 50);
