5198bfa965f7f5040bbae348123a68ee248cd96c
AbhishekSondkar <30792208+AbhishekSondkar@users.noreply.github.com>
2017-08-11 12:57:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,13 @@ var element = document.getElementById('main');
 
 element.innerHTML = 'new value';
 
+var img = document.getElementById('madi');
+
+var marginLeft = 0;
+function moveLeft()
+{
+    marginLeft = marginLeft + 1;
+}
+img.onclick = function (){
+    var interval = setInterval(moveLeft , 50);
+}
